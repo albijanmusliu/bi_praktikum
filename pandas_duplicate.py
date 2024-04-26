6324eafcb74f5e993b67905195c4254a11e999b3
albijanmusliu <62690780+albijanmusliu@users.noreply.github.com>
2024-04-26 11:44:50 +0200

    test

--- pandas_duplicate.py
+++ pandas_duplicate.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 # Load data
-data = pd.read_tsv('C:\Users\TDGCBUF\Desktop\THI\4_Semester\BI_und_Datenanalyse\error_240410\title_basics.tsv')
+data = pd.read_csv(r'C:\Users\TDGCBUF\Desktop\THI\4_Semester\BI_und_Datenanalyse\error_240410\title.basics.tsv', sep=';', header=0)
+#data = pd.read_csv(r'C:\Users\TDGCBUF\Desktop\albi.csv')
+
 # Split text into a list
 data['genres'] = data['genres'].str.split(',')
 # Convert list into multiple rows
-data = data.explode('Skills')
-print(data)
+data = data.explode('genres')
+print(data.head())
